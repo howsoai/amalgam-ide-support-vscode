2b30a313d6433a2e71b65dd1a4f156ea9d628dcb
Cade Mack <24661281+cademack@users.noreply.github.com>
2024-07-16 15:50:53 -0400

    regex pattern improvement, vscode find files

--- src/language/definitions.ts
+++ src/language/definitions.ts
@@ -1,6 +1,5 @@
 import * as vscode from "vscode";
 import * as fs from "fs";
-import * as path from "path";
 
 export class AmalgamDefinitionProvider implements vscode.DefinitionProvider {
   async provideDefinition(
@@ -23,30 +22,34 @@ export class AmalgamDefinitionProvider implements vscode.DefinitionProvider {
         word = "!" + word;
       }
     }
-    const definitionPattern = new RegExp(`#${word}`);
+    const definitionPattern = new RegExp(`^[^;]+#${word}`);
 
     const workspaceFolders = vscode.workspace.workspaceFolders;
     if (!workspaceFolders) {
       return undefined;
     }
 
-
     const workspaceFolder = vscode.workspace.getWorkspaceFolder(document.uri);
     if (!workspaceFolder) {
       return undefined;
     }
-    const files = await this.getAllAmlgFiles(workspaceFolder.uri.fsPath);
+    const files = await vscode.workspace.findFiles(
+      new vscode.RelativePattern(workspaceFolder.uri, "**/**.amlg"),
+      undefined,
+      undefined,
+      token
+    );
 
     for (const file of files) {
       if (token.isCancellationRequested) {
         return undefined;
       }
 
-      const content = await this.readFile(file);
+      const content = await this.readFile(file.fsPath);
       const lines = content.split("\n");
       for (let i = 0; i < lines.length; i++) {
         if (definitionPattern.test(lines[i])) {
-          const definitionUri = vscode.Uri.file(file);
+          const definitionUri = vscode.Uri.file(file.fsPath);
           const definitionPosition = new vscode.Position(i, lines[i].indexOf(`#${word}`));
           return new vscode.Location(definitionUri, definitionPosition);
         }
@@ -54,22 +57,6 @@ export class AmalgamDefinitionProvider implements vscode.DefinitionProvider {
     }
 
     return undefined;
-  }
-
-  private async getAllAmlgFiles(dir: string): Promise<string[]> {
-    const files: string[] = [];
-    const entries = await fs.promises.readdir(dir, { withFileTypes: true });
-
-    for (const entry of entries) {
-      const fullPath = path.join(dir, entry.name);
-      if (entry.isDirectory()) {
-        files.push(...(await this.getAllAmlgFiles(fullPath)));
-      } else if (entry.isFile() && path.extname(entry.name) === ".amlg") {
-        files.push(fullPath);
-      }
-    }
-
-    return files;
   }
 
   private async readFile(filePath: string): Promise<string> {
